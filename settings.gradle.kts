8758c6d04a5d065a3322cb9271b884610f3199ea
selimdawa <selimdawa@gmail.com>
2026-06-24 02:25:49 +0300

    Kotlin Fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,9 +11,6 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -26,4 +23,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Pdf Reader - Little App"
 include(":app")
- 
